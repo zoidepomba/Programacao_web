e6a78a53ca5b865bf7cd265f9547f390fba20791
Guilherme Felipe Santos <guilherme.fsantos@zup.com.br>
2021-08-25 21:32:15 -0300

    primeira aula

--- Aula_1_bruno/src/br/com/aula1/Main.java
+++ Aula_1_bruno/src/br/com/aula1/Main.java
@@ -1,11 +1,27 @@
 package br.com.aula1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.printf("ola mundo");
 
+		Scanner mensagem = new Scanner(System.in);
+
+		int valor1, valor2;
+
+		System.out.println("Digite sua primeira numero: ");
+		valor1 = mensagem.nextInt();
+
+		System.out.println("Digite sua primeira numero: ");
+		valor2 = mensagem.nextInt();
+
+
+		if (valor1 > valor2) {
+			System.out.print(valor1);
+		} else {
+			System.out.print(valor2);
+		}
 	}
 
 }
